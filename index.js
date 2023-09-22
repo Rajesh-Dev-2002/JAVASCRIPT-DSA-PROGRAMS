a30a6738ad13ad29c69e44e192768ae611be068e
Rajesh Kumar Mohanta <119045660+Rajesh7853814223@users.noreply.github.com>
2023-09-22 12:51:00 +0530

    Add files via upload
    Mapping of the Student data with map function

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
-let num =[1,2,3,4,5,6];
+// let num =[1,2,3,4,5,6];
 
-let x = num.map(function(ar){
-    return ar*2;
+// let x = num.map((ar)=>{
+//     return ar*2;
+// })
+
+// console.log(x);
+
+let student = [
+    {
+        name:'Rajesh',
+        marks:96
+    },
+    {
+        name:'Rakesh',
+        marks:85
+    },
+    {
+        name:'Tony Stark',
+        marks:78
+    },
+]
+
+let mapping = student.map((er)=>{
+    return er.marks/10;
 })
 
-console.log(x);
+console.log(mapping);
